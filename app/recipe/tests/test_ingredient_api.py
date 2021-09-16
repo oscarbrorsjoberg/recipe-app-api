08a2ed58cb8b09de46691b95c091ca08b2654714
oscar <oscarbror.sjoberg@gmail.com>
2021-09-16 22:16:27 +0200

    temp

---
+++ app/recipe/tests/test_ingredient_api.py
@@ -0,0 +1,64 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Ingredient
+
+from recipe.serializers import IngredientSerializer
+
+INGREDIENT_URL = reverse('recipe:ingredient-list')
+
+
+class PublicIngredientApiTestCase(TestCase):
+    ''' Test the publicly availibe ingredient API '''
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        ''' Test that login is required for retrieving ingredient '''
+        res = self.client.get(INGREDIENT_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+class PrivateIngredientApiTestCase(TestCase):
+    ''' Test the private ingredients API'''
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            'test@testor.com'
+            'testpass'
+        )
+
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_ingredient_list(self):
+        ''' Test retrieving a list of ingredients '''
+        Ingredient.objects.create(user=self.user, name='Kale')
+        Ingredient.objects.create(user=self.user, name='Salt')
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(Ingredients, many=True)
+        self.assertEqual(res.staus_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_ingredient_limited_to_user(self):
+        ''' Test that ingredients are limited for auth user'''
+        user = get_user_model().objects.create_user(
+            'other@testor.com',
+            'testpass'
+        )
+
+
+
+
+
+
+
+
+    )

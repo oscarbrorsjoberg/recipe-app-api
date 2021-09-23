1961d6c9ab88ff3d5f4d039e82c2de13d7fb1e64
oscar <oscarbror.sjoberg@gmail.com>
2021-09-23 21:11:30 +0200

    get recipe detail

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -28,6 +28,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         queryset=Ingredient.objects.all()
     )
 
+    # Returns only the id of the tags associated to recipe
     tags = serializers.PrimaryKeyRelatedField(
         many=True,
         queryset=Tag.objects.all()
@@ -40,3 +41,9 @@ class RecipeSerializer(serializers.ModelSerializer):
                   'price', 'link')
 
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    ''' Serialize a recipe detial '''
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)

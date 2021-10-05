33c7b9b42434ae0b150e717e3ca8b83d9ebf120a
oscar <oscarbror.sjoberg@gmail.com>
2021-10-05 22:45:15 +0200

    adding test for recipe image

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,16 @@
+import uuid
+import os
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                     PermissionsMixin
 from django.conf import settings  # to import from settings file
+
+
+def recipe_image_file_path(instance, filename):
+    ''' Generate file path for new recipe image'''
+    _, ext = os.path.splitext(filename)
+    filename = f"{uuid.uuid4()}{ext}"
+    return os.path.join('uploads/recipe/', filename)
 
 
 # Create your models here.
@@ -78,6 +87,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     ingredients = models.ManyToManyField('Ingredient')
     tags = models.ManyToManyField('Tag')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title

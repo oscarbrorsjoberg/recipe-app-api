2af68d8cb9efd75f0be2994f41e15be3e99c7d0d
oscar <oscarbror.sjoberg@gmail.com>
2021-11-06 19:46:34 +0100

    removing frontend python app

--- app/frontend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index
-
-urlpatterns = [
-    path('', index),
-    path('recipe', index),
-    path('create', index),
-    path('recipes/<str:recipeId>', index),
-]
